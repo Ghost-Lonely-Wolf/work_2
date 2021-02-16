609a0eccf89cfc7fc990866d585bb40afd1c91e3
Ghost-Lonely-Wolf <76937039+Ghost-Lonely-Wolf@users.noreply.github.com>
2021-02-17 02:10:53 +1100

    Delete work_3_2.py

--- work_3_2.py
+++
@@ -1,9 +0,0 @@
-def print_id(**kwarg):
-    return ' '.join(kwarg.values())
-
-print(print_id(name=input('Введите своё имя '),
-    surname=input('Введите свою фамилию '),
-    date=input('Введите свой год рождения '),
-    city=input('Введите свой город проживания '),
-    email=input('Введите свой email '),
-    number=input('Введите свой номер телефона ')))
