8b57d1ecfed171682e138332f7ded76390639b2d
Ghost-Lonely-Wolf <76937039+Ghost-Lonely-Wolf@users.noreply.github.com>
2021-02-20 01:28:55 +1100

    Delete work_3_3.py

--- work_3_3.py
+++
@@ -1,6 +0,0 @@
-def sum_max(x, y, z):
-    my_func = [x, y, z]
-    my_func.remove(min(my_func))
-    return sum(my_func)
-print(sum_max(int(input('Введите первое число ')), int(input('Введите второе число ')),
-      int(input('Введите третье число '))))
