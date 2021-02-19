e8a9d7d9b484459d40d0f4d4a950feaa2fd98974
Ghost-Lonely-Wolf <76937039+Ghost-Lonely-Wolf@users.noreply.github.com>
2021-02-20 01:31:23 +1100

    Delete work_4_4.py

--- work_4_4.py
+++
@@ -1,4 +0,0 @@
-from random import randint
-my_list = [randint(0, 15) for _ in range(20)]
-new_list = [el for el in my_list if my_list.count(el) == 1]
-print(f'Изначальный список\n{my_list}\nНовый список\n {new_list}')
