112fb04985caa2062391868485b7aa833c5f552c
Alexandr <shulyak.san@mail.ru>
2021-03-05 21:19:39 +1100

    did homework

--- Homework 8/work_8_3.py
+++ Homework 8/work_8_3.py
@@ -3,10 +3,6 @@ class Error:
         self.my_list = []
 
     def my_input(self):
-
-        # self.my_list = [int(i) for i in input('Введите значения через пробел ').split()]
-        # val = int(input('Введите значения и нажимайте Enter - '))
-        # self.my_list.append(val)
         while True:
             try:
                 val = int(input('Введите значения и нажимайте Enter - '))
@@ -14,11 +10,11 @@ class Error:
                 print(f'Текущий список - {self.my_list} \n ')
             except:
                 print(f"Недопустимое значение - строка и булево")
-                y_or_n = input(f'Попробовать еще раз? Y/N ')
+                y_or_n = input(f'Попробовать еще раз? Yes/No ')
 
-                if y_or_n == 'Y' or y_or_n == 'y':
+                if y_or_n == 'Yes' or y_or_n == 'yes':
                     print(try_except.my_input())
-                elif y_or_n == 'N' or y_or_n == 'n':
+                elif y_or_n == 'No' or y_or_n == 'no':
                     return f'Вы вышли'
                 else:
                     return f'Вы вышли'
