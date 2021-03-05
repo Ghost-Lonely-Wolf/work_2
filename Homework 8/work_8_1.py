f7f1f4c1c74036a803fe4c5ea754aa436fd8a8d4
Alexandr <shulyak.san@mail.ru>
2021-03-05 21:14:11 +1100

    did homework

---
+++ Homework 8/work_8_1.py
@@ -0,0 +1,37 @@
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2000 <= year <= 2100:
+                    return f'Все правильно'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('25 - 2 - 2021')
+print(today)
+print(today.valid(28, 6, 2060))
+print(today.valid(10, 57, 2030))
+print(today.valid(8, 4, 2150))
+print(today.valid(8, 4, 1480))
